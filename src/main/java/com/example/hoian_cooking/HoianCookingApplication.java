b00e97052c78eaf1b2284f46f358bffafc41497d
Nguyen Duc Thinh <thinhnnguyenndt248@gmail.com>
2026-03-04 23:24:04 +0700

    initial commit

---
+++ src/main/java/com/example/hoian_cooking/HoianCookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoian_cooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoianCookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoianCookingApplication.class, args);
+	}
+
+}
